d4eca921abf82e07b81d828af68d28ca247dce22
Hayden-Haun <haydenhaun@gmail.com>
2021-08-10 09:41:45 -0600

    adding additional JS functionality and object array declarations

--- script.js
+++ script.js
@@ -1,56 +1,109 @@
 //DECLARE A VARIABLE
 
 //Display todays date at the top of the page!!
-var currentTimeVar = moment().format("MM-DD-YYYY");
-$("#currentDay").text("Today's Date: " + currentTimeVar);
+var currentTimeVar = moment();
+var currentTimeFormatVar = moment().format("MM-DD-YYYY");
+$("#currentDay").text("Today's Date: " + currentTimeFormatVar);
 
-console.log(currentTimeVar);
-var savedtext = [];
+// console.log(currentTimeVar);
+//var userTextArray = [];
+var itemListUl = $(".itemListUl li");
 
-//define variables
-var text9 = $(".9text").val();
-var saveStatus9 = false;
+//define Array
 
-var text10 = $(".10text").val();
-var saveStatus10 = false;
-
-var text11 = $(".11text").val();
-var saveStatus11 = false;
-
-var text12 = $(".12text").val();
-saveStatus12 = false;
-
-var text1 = $(".1text").val();
-saveStatus1 = false;
-
-var text2 = $(".2text").val();
-saveStatus2 = false;
-
-var text3 = $(".3text").val();
-saveStatus3 = false;
-
-var text4 = $(".4text").val();
-saveStatus4 = false;
-
-var text5 = $(".5text").val();
-var saveStatus5 = false;
+var userDataArray = [
+  {
+    text: $(".0text").val(),
+    saveStatus: false,
+    momentTime: moment(),
+    class: null,
+  },
+  {
+    text: $(".1text").val(),
+    saveStatus: false,
+    momentTime: moment(),
+    class: null,
+  },
+  {
+    text: $(".2text").val(),
+    saveStatus: false,
+    momentTime: moment(),
+    class: null,
+  },
+  {
+    text: $(".3text").val(),
+    saveStatus: false,
+    momentTime: moment(),
+    class: null,
+  },
+  {
+    text: $(".4text").val(),
+    saveStatus: false,
+    momentTime: moment(),
+    class: null,
+  },
+  {
+    text: $(".5text").val(),
+    saveStatus: false,
+    momentTime: moment(),
+    class: null,
+  },
+  {
+    text: $(".6text").val(),
+    saveStatus: false,
+    momentTime: moment(),
+    class: null,
+  },
+  {
+    text: $(".7text").val(),
+    saveStatus: false,
+    momentTime: moment(),
+    class: null,
+  },
+  {
+    text: $(".8text").val(),
+    saveStatus: false,
+    momentTime: moment(),
+    class: null,
+  },
+];
 
 //event listeners
 
-$("button9").on("click", saveFunction9);
-$("button10").on("click", saveFunction10);
-$("button11").on("click", saveFunction11);
-$("button12").on("click", saveFunction12);
-$("button1").on("click", saveFunction1);
-$("button2").on("click", saveFunction2);
-$("button3").on("click", saveFunction3);
-$("button4").on("click", saveFunction4);
-$("button5").on("click", saveFunction5);
+// $("button9").on("click", saveFunction9);
+// $("button10").on("click", saveFunction10);
+// $("button11").on("click", saveFunction11);
+// $("button12").on("click", saveFunction12);
+// $("button1").on("click", saveFunction1);
+// $("button2").on("click", saveFunction2);
+// $("button3").on("click", saveFunction3);
+// $("button4").on("click", saveFunction4);
+// $("button5").on("click", saveFunction5);
 
-function savefunction9() {
-  //localStorage.setItem("", savedText);
+function init() {
+  var savedArray = JSON.parse(localStorage.getItem("userTextArray"));
+
+  if (savedArray) {
+    userTextArray = savedArray;
+  }
+
+  saveText();
+  displayText();
 }
-// userInputsArray[0].saveStatus = true;
-//event listeners for save buttons, calls saveFunction>>
 
-//function to call switchSave: change save status, store to local storage
+function saveText() {
+  localStorage.setItem("userTextArray", JSON.stringify(userTextArray));
+}
+
+function displayText() {
+  itemListUl.each(function (counter) {
+    var currentRowClass = '".row' + counter + '"';
+    console.log(currentRowClass);
+  });
+}
+
+displayText();
+
+//list-group-item-info --- blue
+//list-group-item-success --- green
+//list-group-item-secondary --- grey
