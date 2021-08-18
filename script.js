d393f19951e28e091fe486543ee0137b8241cd0a
Hayden-Haun <haydenhaun@gmail.com>
2021-08-18 09:51:17 -0600

    Added object array. added function to compare current time to each timeblock and color code accordingly

--- script.js
+++ script.js
@@ -1,108 +1,104 @@
-//DECLARE A VARIABLE
+///Display todays date at the top of the page!!
+let currentTimeVar = moment();
+let currentDateVar = moment().format("MM-DD-YYYY");
+let currentHourVar = Number(moment().format("H"));
 
-//Display todays date at the top of the page!!
-var currentTimeVar = moment();
-var currentTimeFormatVar = moment().format("MM-DD-YYYY");
-$("#currentDay").text("Today's Date: " + currentTimeFormatVar);
+$("#currentDay").text("Today's Date: " + currentDateVar);
 
 // console.log(currentTimeVar);
-//var userTextArray = [];
-var itemListUl = $(".itemListUl li");
-
-//define Array
+let userTextArray = JSON.parse(localStorage.getItem("userTextArray")) || [];
 
 var userDataArray = [
   {
     text: $(".0text").val(),
     saveStatus: false,
-    momentTime: moment(),
-    class: null,
+    momentTime: 9,
+    element: $("#row0"),
   },
   {
     text: $(".1text").val(),
     saveStatus: false,
-    momentTime: moment(),
-    class: null,
+    momentTime: 10,
+    element: $("#row1"),
   },
   {
     text: $(".2text").val(),
     saveStatus: false,
-    momentTime: moment(),
-    class: null,
+    momentTime: 11,
+    element: $("#row2"),
   },
   {
     text: $(".3text").val(),
     saveStatus: false,
-    momentTime: moment(),
-    class: null,
+    momentTime: 12,
+    element: $("#row3"),
   },
   {
     text: $(".4text").val(),
     saveStatus: false,
-    momentTime: moment(),
-    class: null,
+    momentTime: 13,
+    element: $("#row4"),
   },
   {
     text: $(".5text").val(),
     saveStatus: false,
-    momentTime: moment(),
-    class: null,
+    momentTime: 14,
+    element: $("#row5"),
   },
   {
     text: $(".6text").val(),
     saveStatus: false,
-    momentTime: moment(),
-    class: null,
+    momentTime: 15,
+    element: $("#row6"),
   },
   {
     text: $(".7text").val(),
     saveStatus: false,
-    momentTime: moment(),
-    class: null,
+    momentTime: 16,
+    element: $("#row7"),
   },
   {
-    text: $(".8text").val(),
+    text: "Some String Cheese!",
     saveStatus: false,
-    momentTime: moment(),
-    class: null,
+    momentTime: 17,
+    element: $("#row8"),
   },
 ];
 
-//event listeners
+//THIS FUNCTION COMPARES THE CURRENT HOUR TO EACH HOUR ON THE PLANNER. IT WILL COLOR EACH TIMEBLOCK ACCORDINGLY
+function compareTimes() {
+  console.log(currentHourVar);
 
-// $("button9").on("click", saveFunction9);
-// $("button10").on("click", saveFunction10);
-// $("button11").on("click", saveFunction11);
-// $("button12").on("click", saveFunction12);
-// $("button1").on("click", saveFunction1);
-// $("button2").on("click", saveFunction2);
-// $("button3").on("click", saveFunction3);
-// $("button4").on("click", saveFunction4);
-// $("button5").on("click", saveFunction5);
+  userDataArray.forEach(function (timeBlock) {
+    console.log(timeBlock.momentTime);
+    if (currentHourVar < timeBlock.momentTime) {
+      timeBlock.element.addClass("list-group-item-info");
+    } else if (currentHourVar === timeBlock.momentTime) {
+      timeBlock.element.addClass("list-group-item-success");
+    } else {
+      timeBlock.element.addClass("list-group-item-secondary");
+    }
+  });
+}
 
 function init() {
-  var savedArray = JSON.parse(localStorage.getItem("userTextArray"));
-
-  if (savedArray) {
-    userTextArray = savedArray;
-  }
-
+  compareTimes();
   saveText();
   displayText();
 }
 
 function saveText() {
   localStorage.setItem("userTextArray", JSON.stringify(userTextArray));
+  //ADD USER TEXT ARRAY TO OBJECT ARRAY
 }
 
 function displayText() {
-  itemListUl.each(function (counter) {
-    var currentRowClass = '".row' + counter + '"';
-    console.log(currentRowClass);
+  userDataArray.forEach(function (timeBlock) {
+    timeBlock.element.find("textarea").val(timeBlock.text);
   });
 }
 
-displayText();
+init();
 
 //list-group-item-info --- blue
 //list-group-item-success --- green
